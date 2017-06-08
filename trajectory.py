a838cdcc17a7e35bfaaea6c35942a2da3d4d7f56
AndreaPerrello <perrello.a94@gmail.com>
2017-06-08 16:50:07 +0200

    [trajectory.py] Codice riformattato e ottimizzato. Spostato il metodo "draw" per incoerenza.

--- trajectory.py
+++ trajectory.py
@@ -7,27 +7,20 @@ from filterpy.common import Q_discrete_white_noise
 class Trajectory():
     globID = 0
 
-    def __init__(self, r, gti):
+    def __init__(self, run, gti):
         self.id = Trajectory.globID
         Trajectory.globID += 1
 
-        self.run = r
+        self.run = list(run)
         self.points = []
         self.gti = gti
         self.ci = -1
         self.prefixSum = [0.0]
 
-    # filtra la corsa eliminando i punti che sono troppo vicini tra di loro - DA ELIMINARE
-    def clean_run(self, param=0.5):
-        index = len(self.run) - 1
-        while index > 0:
-            dist = euclid_dist((self.run[index].x, self.run[index].y), (self.run[index - 1].x, self.run[index - 1].y))
-            if dist <= param:
-                self.run.remove(self.run[index])
-            index -= 1
+        self.build()
 
-    # filtra la corsa eliminando i punti che sono troppo vicini tra di loro
     def clean(self, param=0.9, dist=3.):
+        "Filtra la corsa eliminando i punti che sono troppo vicini tra di loro"
         run = self.getPoints()
         i = 0
         while i < len(run) - 1:
@@ -41,31 +34,14 @@ class Trajectory():
                     self.points.remove(run[ii])
                 ii += 1
             i += 1
+        # Aggiorna la lunghezza della corsa
+        self.setPrefixSum()
 
-    # filtra la corsa attraverso un filtro di Kalman - DA ELIMINARE
-    def kalman_filter(self):
-        # crea il filtro di Kalman
-        f = KalmanFilter(dim_x=2, dim_z=2)
-        # inizializza il filtro
-        f.x = np.array([self.run[0].x, self.run[0].y])
-        index = 0
-        while index < len(self.run) - 1:
-            f.F = np.array([[1, 0], [0, 1]])  # state transition matrix
-            f.H = np.array([[1, 0], [0, 1]])  # Measurement function
-            f.P *= 1000.  # covariance matrix
-            f.R = np.array([[1, 0], [0, 1]])  # state uncertainty
-            f.Q = Q_discrete_white_noise(2, 1., 1.)  # process uncertainty
-            f.predict()
-            f.update([self.run[index + 1].x, self.run[index + 1].y])
-            self.run[index + 1].x = f.x[0]
-            self.run[index + 1].y = f.x[1]
-            index += 1
-
-    # filtra la corsa attraverso un filtro di Kalman
     def filter(self):
-        # crea il filtro di Kalman
+        "Filtra la corsa attraverso un filtro di Kalman"
+        # Crea il filtro di Kalman
         f = KalmanFilter(dim_x=2, dim_z=2)
-        # inizializza il filtro
+        # Inizializza il filtro
         f.x = np.array(self.points[0])
         index = 0
         while index < len(self.points) - 1:
@@ -79,10 +55,15 @@ class Trajectory():
             self.points[index + 1] = f.x
             index += 1
 
-    def addPoint(self, p):
-        self.points.append(p)
+    def build(self):
+        "Costruisce i punti della traiettoria"
+        for cart in self.run:
+            self.addPoint((cart.x, cart.y))
+        # Aggiorna la lunghezza della corsa
+        self.setPrefixSum()
 
     def setPrefixSum(self):
+        "Calcola e aggiorna la lunghezza della traiettoria"
         if len(self.points) > 0:
             index = 0
             while index < len(self.points) - 1:
@@ -90,11 +71,14 @@ class Trajectory():
                                       euclid_dist(self.points[index + 1], self.points[index]))
                 index += 1
 
-    def getPoints(self):
-        return self.points
-
     def getPrefixSum(self):
         return self.prefixSum
+
+    def addPoint(self, p):
+        self.points.append(p)
+
+    def getPoints(self):
+        return self.points
 
     def groundTruth(self):
         return self.gti
@@ -107,19 +91,6 @@ class Trajectory():
 
     def length(self):
         return self.prefixSum[len(self.prefixSum) - 1]
-
-    def draw(self, widget, color):
-        xlast, ylast = None, None
-        for p in self.points:
-            # paint a point
-            widget.create_oval(p[0] * widget.scale - 3, p[1] * widget.scale - 3, p[0] * widget.scale + 3,
-                               p[1] * widget.scale + 3, fill=color)
-            # paint a line
-            if xlast is not None and ylast is not None:
-                widget.create_line(xlast * widget.scale, ylast * widget.scale, p[0] * widget.scale, p[1] * widget.scale,
-                                   smooth=True)
-            xlast = p[0]
-            ylast = p[1]
 
     @staticmethod
     def decGlobID():
@@ -141,6 +112,37 @@ class Trajectory():
     def __len__(self):
         return len(self.points)
 
+    ##################
+    ## DA ELIMINARE ##
+    ##################
+
+    # Filtra la corsa eliminando i punti che sono troppo vicini tra di loro
+    def clean_run(self, param=0.5):
+        index = len(self.run) - 1
+        while index > 0:
+            dist = euclid_dist((self.run[index].x, self.run[index].y), (self.run[index - 1].x, self.run[index - 1].y))
+            if dist <= param:
+                self.run.remove(self.run[index])
+            index -= 1
+
+    # Filtra la corsa attraverso un filtro di Kalman
+    def kalman_filter(self):
+        # crea il filtro di Kalman
+        f = KalmanFilter(dim_x=2, dim_z=2)
+        # inizializza il filtro
+        f.x = np.array([self.run[0].x, self.run[0].y])
+        index = 0
+        while index < len(self.run) - 1:
+            f.F = np.array([[1, 0], [0, 1]])  # state transition matrix
+            f.H = np.array([[1, 0], [0, 1]])  # Measurement function
+            f.P *= 1000.  # covariance matrix
+            f.R = np.array([[1, 0], [0, 1]])  # state uncertainty
+            f.Q = Q_discrete_white_noise(2, 1., 1.)  # process uncertainty
+            f.predict()
+            f.update([self.run[index + 1].x, self.run[index + 1].y])
+            self.run[index + 1].x = f.x[0]
+            self.run[index + 1].y = f.x[1]
+            index += 1
 
 if __name__ == "__main__":
     # Test prefix sum
