cdf07b448bede727b2098ef3092d342fdc2c8fba
AndreaPerrello <perrello.a94@gmail.com>
2017-06-13 12:05:19 +0200

    [trajectory.py] Rimossi test, funzioni e parametri inutilizzati

--- trajectory.py
+++ trajectory.py
@@ -7,13 +7,12 @@ from filterpy.common import Q_discrete_white_noise
 class Trajectory():
     globID = 0
 
-    def __init__(self, run, gti):
+    def __init__(self, run):
         self.id = Trajectory.globID
         Trajectory.globID += 1
 
         self.run = list(run)
         self.points = []
-        self.gti = gti
         self.ci = -1
         self.prefixSum = [0.0]
         self.track = -1
@@ -28,7 +27,7 @@ class Trajectory():
             ii = i + 1
             dist_i = 0
             while ii < len(run) - 1:
-                dist_i_ii = euclid_dist(run[i], run[ii])
+                dist_i_ii = euclidean_distance(run[i], run[ii])
                 # dist_ii = euclid_dist(run[ii-1], run[ii])
                 # dist_i += dist_ii
                 if dist_i_ii < param:
@@ -70,7 +69,7 @@ class Trajectory():
             index = 0
             while index < len(self.points) - 1:
                 self.prefixSum.append(self.prefixSum[len(self.prefixSum) - 1] +
-                                      euclid_dist(self.points[index + 1], self.points[index]))
+                                      euclidean_distance(self.points[index + 1], self.points[index]))
                 index += 1
 
     def getPrefixSum(self):
@@ -81,9 +80,6 @@ class Trajectory():
 
     def getPoints(self):
         return self.points
-
-    def groundTruth(self):
-        return self.gti
 
     def getClusterIdx(self):
         return self.ci
@@ -104,7 +100,6 @@ class Trajectory():
 
     def __str__(self):
         str = "=== Trajectory ===\n"
-        str += "ground truth: %d\n" % self.gti
         str += "cluster: %d\n" % self.ci
         for p in self.points:
             str += repr(p) + ", "
@@ -114,57 +109,8 @@ class Trajectory():
     def __len__(self):
         return len(self.points)
 
-    ##################
-    ## DA ELIMINARE ##
-    ##################
-
-    # Filtra la corsa eliminando i punti che sono troppo vicini tra di loro
-    def clean_run(self, param=0.5):
-        index = len(self.run) - 1
-        while index > 0:
-            dist = euclid_dist((self.run[index].x, self.run[index].y), (self.run[index - 1].x, self.run[index - 1].y))
-            if dist <= param:
-                self.run.remove(self.run[index])
-            index -= 1
-
-    # Filtra la corsa attraverso un filtro di Kalman
-    def kalman_filter(self):
-        # crea il filtro di Kalman
-        f = KalmanFilter(dim_x=2, dim_z=2)
-        # inizializza il filtro
-        f.x = np.array([self.run[0].x, self.run[0].y])
-        index = 0
-        while index < len(self.run) - 1:
-            f.F = np.array([[1, 0], [0, 1]])  # state transition matrix
-            f.H = np.array([[1, 0], [0, 1]])  # Measurement function
-            f.P *= 1000.  # covariance matrix
-            f.R = np.array([[1, 0], [0, 1]])  # state uncertainty
-            f.Q = Q_discrete_white_noise(2, 1., 1.)  # process uncertainty
-            f.predict()
-            f.update([self.run[index + 1].x, self.run[index + 1].y])
-            self.run[index + 1].x = f.x[0]
-            self.run[index + 1].y = f.x[1]
-            index += 1
-
-if __name__ == "__main__":
-    # Test prefix sum
-    t1 = Trajectory(0)
-
-    t1.addPoint((0, 0))
-    t1.addPoint((1, 1))
-    t1.addPoint((2, 2))
-    t1.addPoint((3, 3))
-
-    ps = [i * sqrt(2) for i in range(4)]
-    assert (ps == t1.prefixSum)
-
-    l = t1.length()
-    assert (l == 3 * sqrt(2))
-
-    print("TEST END")
-
 
 # calcola la distanza euclidea tra due punti p1 e p2
-def euclid_dist(p1, p2):
+def euclidean_distance(p1, p2):
     assert (len(p1) == len(p2))
     return sqrt(sum([((p1[i] - p2[i])) ** 2 for i in range(len(p1))]))
